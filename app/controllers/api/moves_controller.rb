2de4f00c20d8ac0a4f527012bc0482ed57050422
Fabio Xavier <fabio.g.xavier33@gmail.com>
2021-12-02 15:39:37 -0700

    backend done and tested

--- app/controllers/api/moves_controller.rb
+++ app/controllers/api/moves_controller.rb
@@ -1,2 +1,48 @@
 class Api::MovesController < ApplicationController
+
+    before_action :set_champ
+    before_action :set_move, only: [:destroy, :show, :update]
+    
+    def index
+        render json: @champ.moves 
+    end
+
+    def create
+        @move = @champ.moves.new(set_params)
+        if @move.save
+            render json: @move
+        else
+            render json: {errors: @move.errors}, status: 422 # :unprocessable_entity
+        end
+    end
+
+    def show
+        render json: @move
+    end
+
+    def update
+        if @move.update(set_params)
+            render json: @move
+        else
+            render json: {errors: @move.errors}, status: 422 ## I only have these if i want to do error handling and validations
+        end
+    end
+
+    def destroy
+        render json: @move.destroy
+    end
+
+    private
+
+    def set_params
+        params.require(:move).permit(:name, :desc, :dmg, :element)
+    end
+
+    def set_move
+        @move = @champ.moves.find(params[:id]) 
+    end
+
+    def set_champ
+        @champ = Champ.find(params[:champ_id])
+    end
 end
